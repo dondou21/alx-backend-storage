fa0be37656e69d2a9b32010d946ae4fb9377346d
dondou21 <dondouabiyi@gmail.com>
2024-08-07 14:41:55 +0200

    updated

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- 'email' string (255 characters), nevr null and unique, 'name' string(255 characters. If the table already exist, the script should not fall
 
 DROP TABLE IF EXIST users;
-CREATE TABLE userss(
+CREATE TABLE users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255)NOT NULL UNIQUE,
 	name VARCHAR(255) 
